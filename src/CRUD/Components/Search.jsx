b7d097669307d41fb8635f308fa6a31ec48fa473
Ramroop26 <ramroop26io071@satiengg.in>
2025-08-11 11:27:51 +0530

    Your specific commit message for the submodule changes

--- src/CRUD/Components/Search.jsx
+++ src/CRUD/Components/Search.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
 const Search=()=>{
+    const [rollno, setRollno] = useState("");
+    const [mydata, setMydata] = useState([]);
+    const handleSubmit=async()=>{
+        let api = `http://localhost:3000/students/?rollno=${rollno}`;
+        const response = await axios(api);
+        console.log(response.data);
+        setMydata(response.data);
+    }
+
+    const ans = mydata.map((key) =>{
+        return(
+            <>
+            <tr>
+                <th>{key.rollno}</th>
+                <th>{key.name}</th>
+                <th>{key.city}</th>
+                <th>{key.fees}</th>
+                
+            </tr>
+            </>
+        )
+    })
     return(
         <>
-        <h1>Welcome to Search Page</h1>
+        <h1>Search Page</h1>
+        Enter Rollno : <input type="text" value={rollno}  onChange={(e) =>{e.target.value}}/>
+        <button onClick={handleSubmit}>Search</button>
+        <hr />
+
+        <table width="600" border="1">
+            <tr>
+                <th>Rollno</th>
+                <th>Name</th>
+                <th>City</th>
+                <th>Fees</th>
+            </tr>
+            {ans}
+        </table>
         </>
     )
 
